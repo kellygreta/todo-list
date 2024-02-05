ba67184b6c9d539a56dcfabb307403912059ecbf
kellygreta <greta.vigano97@gmail.com>
2024-02-05 17:07:03 +0100

    first set up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
-<div>
-    <h2 class="text-3xl text-center p-4 bg-slate-300"> TO DO list </h2>
+<div class="flex flex-col justify-center p-4">
+    <h2 class="text-3xl bg-slate-300"> TO DO list </h2>
+    <!-- TODO form component -->
+    <div class="flex-col flex">
+        <p >Enter your todo here:</p>
+        <div class="flex">
+            <div >
+                <input class=" border-2 border-blue-500 rounded-md" type="text">
+                 <!-- TODO button component -->
+                <button class="bg-blue-500 rounded-md">Enter</button>
+            </div>
+        </div>
+    </div>
+    <!-- TODO todo component -->
+    <div class="flex">
+        <input type="checkbox">
+        <h4>first todo</h4>
+        <div>
+            <button class="bg-blue-500 rounded-md">Edit</button>
+            <button class="bg-blue-500 rounded-md">Delete</button>
+        </div>
+    </div>
 </div>
